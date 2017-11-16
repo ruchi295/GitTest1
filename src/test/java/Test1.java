e4fce942bbed70f11b2aa48b3cdebb34b161a7ad
ruchi295 <ruchijhamb9@gmail.com>
2017-11-16 15:41:25 +0530

    test message branch1

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String... args)
     {
+        System.out.println("Editing for same line of code");
         System.out.println("Hello");
         System.out.printf("This is my first test program");
     }
---
+++ src/test/java/Test3.java
@@ -0,0 +1,6 @@
+public class Test3 {
+    public static void main (String... arg)
+    {
+        System.out.println("Editing for same line of code");
+    }
+}

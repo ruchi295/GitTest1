317ab227268ea5550934e219cdf29754aff1af2d
ruchi295 <ruchijhamb9@gmail.com>
2017-11-16 16:14:05 +0530

    test

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,8 +2,7 @@ public class Test1 {
     public static void main(String... args)
     {
         System.out.println("00000000000000000000000000000000000000000000000000000000000000000000000");
-        System.out.println("Hello");
+        System.out.println("Hello world1");
         System.out.printf("This is my first test program");
-        System.out.println("Hello1");
     }
 }
